9617f4a3893ed0c8a19a1e5db7922b76e16db5fa
Camel-Blue <33599420+Camel-Blue@users.noreply.github.com>
2024-05-22 22:50:41 +0300

    removed unneeded files

--- csmas/Monitoring.py
+++ csmas/Monitoring.py
@@ -8,22 +8,6 @@ import asyncio
 import requests
 
 
-
-
-"""while True:
-	cpu_pre = psutil.cpu_percent()
-	cpu_cores = psutil.cpu_count(logical=True)
-	cpu_freq = psutil.cpu_freq().current
-	sys_load = psutil.getloadavg()
-	ram = psutil.virtual_memory().percent
-	mem_pre_used = psutil.disk_usage('/').percent
-	up_time_sec = datetime.datetime.now() - datetime.datetime.fromtimestamp(psutil.boot_time())
-	up_time = str(up_time_sec).split('.')[0]
-	b_time = datetime.datetime.fromtimestamp(psutil.boot_time()).strftime("%Y-%m-%d %H:%M:%S")
-	print(f'CPU%: {cpu_pre}, Cores: {cpu_cores}, Freq: {cpu_freq}, sys load: {sys_load}')
-	print(f'mem: {ram}%, disk% used: {mem_pre_used}%, BOOT TIME: {b_time}, Up_time: {up_time}')
-	time.sleep(1)
-"""
 cpu_name = cpuinfo.get_cpu_info()['brand_raw']
 
 def get_size(bytes, suffix="B"):
