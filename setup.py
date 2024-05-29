4323832e78a78b8a85c64ac3e38ee66388a3db6e
Camel-Blue <33599420+Camel-Blue@users.noreply.github.com>
2024-05-29 22:35:29 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 
 setup(
+    long_description="""This is a simple programm to get updates of server's status on Discord""",
     name='csmas',
     version='0.0.1',
     packages=['csmas'],
